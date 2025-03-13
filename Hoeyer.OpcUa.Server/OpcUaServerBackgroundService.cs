35807ac9c46b40cac9088a8834093d489aeeab19
rasmus <rasmus.hoeyer.hansen@gmail.com>
2025-03-13 19:28:21 +0100

    Add hosted service project

---
+++ Hoeyer.OpcUa.Server/OpcUaServerBackgroundService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Hoeyer.OpcUa.Server;
+
+public sealed class OpcUaServerBackgroundService : BackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public OpcUaServerBackgroundService(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        OpcUaEntityServerFactory factory = _serviceProvider.GetRequiredService<OpcUaEntityServerFactory>();
+        IStartableEntityServer server = factory.CreateServer();
+        await server.StartAsync();
+        await Task.Delay(Timeout.Infinite, stoppingToken);
+    }
+}
