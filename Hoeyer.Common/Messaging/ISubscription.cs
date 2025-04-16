c9924261379cd0dc52b82a9c5120d1a6816be6c9
rasmus <rasmus.hoeyer.hansen@gmail.com>
2025-04-16 10:07:31 +0200

    [Refactor] Renamed Subscription to EntitySubscription.cs

---
+++ Hoeyer.Common/Messaging/ISubscription.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hoeyer.Common.Messaging;
+
+public interface ISubscription : IDisposable
+{
+    Guid SubscriptionId { get; }
+    bool IsCancelled { get; }
+    bool IsPaused { get; }
+    void Unpause();
+    void Pause();
+}
