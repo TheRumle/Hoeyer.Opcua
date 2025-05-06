2a43b0dd813755627182aef3b33ce29afc116f32
rasmus <rasmus.hoeyer.hansen@gmail.com>
2025-05-06 20:59:14 +0200

    Chore: Better comments

--- Hoeyer.OpcUa.Client/Api/Browsing/IEntityBrowser.cs
+++ Hoeyer.OpcUa.Client/Api/Browsing/IEntityBrowser.cs
@@ -12,9 +12,9 @@ public interface IEntityBrowser
     (IEntityNode? node, DateTime timeLoaded)? LastState { get; }
     
     /// <summary>
-    /// Traverses the node tree of the OpcUa server to starting from the root. The traversal halts when the node representing the entity has been found, browsed and read. A full tree traversal will be skipped if the entity node has already been found previously.
+    /// Traverses the node tree of the OpcUa server to starting from the root. The traversal halts when the node representing the entity has been found, browsed and read. A full tree traversal will be skipped if the entity node has already been found previously - instead traversal will begin from the root of the node representing the entity.
     /// </summary>
     /// <param name="cancellationToken">Token to cancel the browse</param>
-    /// <returns>An IEntityNode representing the structure</returns>
+    /// <returns>An IEntityNode representing the structure of the entity.</returns>
     Task<IEntityNode> BrowseEntityNode(CancellationToken cancellationToken = default);
 }
--- Hoeyer.OpcUa.Client/Application/Browsing/EntityBrowser.cs
+++ Hoeyer.OpcUa.Client/Application/Browsing/EntityBrowser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,11 +22,10 @@ namespace Hoeyer.OpcUa.Client.Application.Browsing;
 /// <summary>
 /// A client for browsing an entity - traverses the node tree from root using the <paramref name="traversalStrategy"/> and until a match is found using <paramref name="identityMatcher"/> - the match is deemed to be the entity node.
 /// If the node has already been found previously, the tree is not traversed again.
-/// BrowseById specific Object node representing an assigned entity. Holds BrowseById reference to the ObjectNode (once the first browse has been done successfully)
 /// </summary>
 /// <param name="logger">A logger to log browse exceptions and diagnostics</param>
 /// <param name="traversalStrategy">A strategy for traversing the node tree</param>
-/// <param name="identityMatcher">True if the provided <see cref="ReferenceDescription"/> is a description for the entity - if null, equality between the name of the <typeparamref name="TEntity"/> and the browse name of nodes is used.</param>
+/// <param name="identityMatcher">True if the provided <see cref="ReferenceDescription"/> is a description for the entity</param>
 /// <typeparam name="TEntity">The entity the EntityBrowser is assigned to</typeparam>
 [OpcUaEntityService(typeof(IEntityBrowser<>))]
 public sealed class EntityBrowser<TEntity>(
@@ -46,6 +46,53 @@ public sealed class EntityBrowser<TEntity>(
     
     public (IEntityNode? node, DateTime timeLoaded)? LastState { get; private set; }
 
+    /// <inheritdoc />
+    public async Task<IEntityNode> BrowseEntityNode(CancellationToken cancellationToken = default)
+    {
+        var values = await ReadEntity(cancellationToken);
+        return await ParseToEntity(Session, cancellationToken, values);
+    }
+
+    private async Task<IEntityNode> ParseToEntity(ISession session, CancellationToken cancellationToken, ReadResult values)
+    {
+        var variables = await reader
+            .ReadNodesAsync(session, values.SuccesfulReads.Select(value => value!.NodeId),
+                ct: cancellationToken)
+            .ThenAsync(result => result.SuccesfulReads.OfType<VariableNode>())
+            .ThenAsync( nodes => nodes.ToList());
+
+        var (readValues, _) = await session.ReadValuesAsync(variables.Select(e=>e.NodeId).ToList(), cancellationToken);
+        for (int i = 0; i < readValues.Count; i++)
+        {
+            variables[i]!.Value = readValues[i].WrappedValue;
+        }
+
+        var structure = AssignReadValues(variables);
+        return structure;
+    }
+
+    private IEntityNode AssignReadValues(List<VariableNode> variables)
+    {
+        var index = _entityRoot!.NodeId.NamespaceIndex;
+        var structure = nodeStructureFactory.Create(index);
+        foreach (var state in structure.PropertyStates)
+        {
+            
+            var match = variables.FirstOrDefault(n => n != null && n.NodeId.Equals(state.NodeId));
+            if (match != null) state.Value = match.Value;
+        }
+        LastState = new ValueTuple<IEntityNode, DateTime>(structure, DateTime.Now);
+        return structure;
+    }
+
+    private async Task<ReadResult> ReadEntity(CancellationToken cancellationToken)
+    {
+        _entityRoot ??= await FindEntityRoot(cancellationToken);
+        var descendants = await traversalStrategy.TraverseFrom(_entityRoot.NodeId, Session, cancellationToken).Collect();
+        var values = await reader.ReadNodesAsync(Session, descendants.Select(e=>e.NodeId), ct: cancellationToken);
+        return values;
+    }
+    
     private async Task<Node> FindEntityRoot(CancellationToken cancellationToken = default)
     {
         return await logger.LogWithScopeAsync(new
@@ -62,56 +109,5 @@ public sealed class EntityBrowser<TEntity>(
 
             return await reader.ReadNodeAsync(Session, r.NodeId, cancellationToken);
         });
-
-    }
-
-    /// <inheritdoc />
-    public async Task<IEntityNode> BrowseEntityNode(CancellationToken cancellationToken = default)
-    {
-        var values = await ReadEntity(cancellationToken);
-        return await ParseToEntity(Session, cancellationToken, values);
-    }
-
-    private async Task<IEntityNode> ParseToEntity(ISession session, CancellationToken cancellationToken, ReadResult values)
-    {
-        var variables = await reader
-            .ReadNodesAsync(session, values.SuccesfulReads.Select(value => value!.NodeId),
-                ct: cancellationToken)
-            .ThenAsync(e => e.SuccesfulReads)
-            .ThenAsync(nodes => nodes.Where(v => v switch
-            {
-                VariableNode => true,
-                _ => false
-            }))
-            .ThenAsync( e => e.Select(node => node as VariableNode!).ToList());
-
-        var (readValues, _) = await session.ReadValuesAsync(variables.Select(e=>e.NodeId).ToList(), cancellationToken);
-        for (int i = 0; i < readValues.Count; i++)
-        {
-            variables[i]!.Value = readValues[i].WrappedValue;
-        }
-
-        var index = _entityRoot!.NodeId.NamespaceIndex;
-        var structure = nodeStructureFactory.Create(index);
-        foreach (var state in structure.PropertyStates)
-        {
-            
-            var match = variables
-                .FirstOrDefault(n => n != null && n.NodeId.Equals(state.NodeId));
-            if (match != null)
-            {
-                state.Value = match.Value;
-            }
-        }
-        LastState = new ValueTuple<IEntityNode, DateTime>(structure, DateTime.Now);
-        return structure;
-    }
-
-    private async Task<ReadResult> ReadEntity(CancellationToken cancellationToken)
-    {
-        _entityRoot ??= await FindEntityRoot(cancellationToken);
-        var descendants = await traversalStrategy.TraverseFrom(_entityRoot.NodeId, Session, cancellationToken).Collect();
-        var values = await reader.ReadNodesAsync(Session, descendants.Select(e=>e.NodeId), ct: cancellationToken);
-        return values;
     }
 }
