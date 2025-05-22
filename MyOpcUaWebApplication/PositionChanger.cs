eb42482cdf06f5f3b9dbaf075cfbbb5897dde531
rasmus <rasmus.hoeyer.hansen@gmail.com>
2025-05-22 21:03:54 +0200

    [Feature, Refactor] Better examples for simulation with return value when calling Position

---
+++ MyOpcUaWebApplication/PositionChanger.cs
@@ -0,0 +1,17 @@
+﻿using Hoeyer.OpcUa.Server.Api;
+
+namespace MyOpcUaWebApplication;
+
+public class PositionChanger(EntityServerStartedMarker marker, IGantryMethods methods) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await marker.ServerRunning();
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
+            Console.WriteLine("Calling the method GetRandomPosition resulted in " +
+                              await methods.GetRandomPosition("q", 2f, []));
+        }
+    }
+}
