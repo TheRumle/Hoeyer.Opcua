1d7162743f7b59e222057e124d13e9a43336449f
rasmus <rasmus.hoeyer.hansen@gmail.com>
2025-05-27 19:42:35 +0200

    [Fix] Analyser now handles HashSets

---
+++ Hoeyer.OpcUa.Core.SourceGeneration/Models/FullyQualifiedTypeName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Hoeyer.OpcUa.Core.SourceGeneration.Models;
+
+public record FullyQualifiedTypeName
+{
+    public FullyQualifiedTypeName(string fullyQualifiedType)
+    {
+        WithoutGlobalPrefix = fullyQualifiedType;
+        WithGlobalPrefix = $"global::{WithoutGlobalPrefix}";
+    }
+
+
+    public FullyQualifiedTypeName(INamedTypeSymbol symbol)
+    {
+        WithoutGlobalPrefix = symbol.ToDisplayString();
+        WithGlobalPrefix = $"global::{WithoutGlobalPrefix}";
+    }
+
+    public string WithoutGlobalPrefix { get; }
+
+    public string WithGlobalPrefix { get; }
+
+    public bool Matches(string other) => WithGlobalPrefix.Equals(other) || WithoutGlobalPrefix.Equals(other);
+}
---
+++ Hoeyer.OpcUa.Core.SourceGeneration/Syntax/SymbolExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+
+namespace Hoeyer.OpcUa.Core.SourceGeneration.Syntax;
+
+public static class SymbolExtensions
+{
+    public static string GetFullNamespace(this INamedTypeSymbol symbol)
+    {
+        INamespaceSymbol? namespaceSymbol = symbol.ContainingNamespace;
+        if (namespaceSymbol == null || namespaceSymbol.IsGlobalNamespace) return string.Empty;
+
+        var parts = new Stack<string>();
+        while (!namespaceSymbol.IsGlobalNamespace)
+        {
+            parts.Push(namespaceSymbol.Name);
+            namespaceSymbol = namespaceSymbol.ContainingNamespace;
+        }
+
+        return string.Join(".", parts);
+    }
+}
