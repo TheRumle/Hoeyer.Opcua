a5220555ca83ce9c36d2dcd9393c893619750d46
rasmus <rasmus.hoeyer.hansen@gmail.com>
2025-06-01 20:02:19 +0200

    [Feature] CurrentEntityStateChannel holds a channel representing all state changes of an entity

---
+++ Hoeyer.OpcUa.Client/Application/Monitoring/CurrentEntityStateChannel.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Channels;
+using Hoeyer.Common.Messaging.Api;
+using Hoeyer.OpcUa.Core;
+
+namespace Hoeyer.OpcUa.Client.Application.Monitoring;
+
+[OpcUaEntityService(typeof(ICurrentEntityStateChannel<>))]
+public sealed class CurrentEntityStateChannel<T> : ICurrentEntityStateChannel<T>
+{
+    private readonly Channel<IMessage<T>> _channel;
+
+    private readonly BoundedChannelOptions _options = new(1)
+    {
+        FullMode = BoundedChannelFullMode.DropOldest,
+        AllowSynchronousContinuations = false,
+        SingleWriter = true
+    };
+
+    public CurrentEntityStateChannel()
+    {
+        _channel = Channel.CreateBounded<IMessage<T>>(_options);
+    }
+
+    public int Capacity => _options.Capacity;
+
+
+    /// <inheritdoc />
+    public void Consume(IMessage<T> message) => _channel.Writer.TryWrite(message);
+
+    public ChannelReader<IMessage<T>> Reader => _channel.Reader;
+}
---
+++ Hoeyer.OpcUa.Client/Application/Monitoring/ICurrentEntityStateChannel.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Channels;
+using Hoeyer.Common.Messaging.Api;
+
+namespace Hoeyer.OpcUa.Client.Application.Monitoring;
+
+public interface ICurrentEntityStateChannel<T> : IMessageConsumer<T>
+{
+    public ChannelReader<IMessage<T>> Reader { get; }
+}
