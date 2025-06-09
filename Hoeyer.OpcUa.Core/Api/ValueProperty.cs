09218deaae285e845c963ab4d51ce2898e102879
rasmus <rasmus.hoeyer.hansen@gmail.com>
2025-06-09 18:33:39 +0200

    [Refactor] Move ValueProperty.cs into its own file

---
+++ Hoeyer.OpcUa.Core/Api/ValueProperty.cs
@@ -0,0 +1,11 @@
+﻿using Opc.Ua;
+
+namespace Hoeyer.OpcUa.Core.Api;
+
+public sealed class ValueProperty(PropertyState state)
+{
+    public string Name { get; } = state.BrowseName.Name;
+    public NodeId NodeId { get; } = state.NodeId;
+    public object Value { get; set; } = state.WrappedValue.Value;
+    public object Handle { get; } = state.Handle;
+}
