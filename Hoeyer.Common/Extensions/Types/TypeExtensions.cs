85b034a47611245d6749dd66cfbdadc806544935
rasmus <rasmus.hoeyer.hansen@gmail.com>
2025-06-15 18:52:49 +0200

    [Feat] TypeExtensions.cs added, ResultExtensions.cs removed

---
+++ Hoeyer.Common/Extensions/Types/TypeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Hoeyer.Common.Extensions.Types;
+
+public static class TypeExtensions
+{
+    public static Type? GetImplementedVersionOfGeneric(this Type t, Type @interface)
+    {
+        return t.GetInterfaces().FirstOrDefault(i =>
+            i.IsGenericType &&
+            i.GetGenericTypeDefinition() == @interface);
+    }
+
+    public static Type? Implements(this Type t, Type @interface)
+    {
+        return t.GetInterfaces().FirstOrDefault(i => i == @interface);
+    }
+}
