fa281a713fc770ca605fd200aeb5ab01d16315ec
rasmus <rasmus.hoeyer.hansen@gmail.com>
2025-06-15 18:52:18 +0200

    [Feat] Simulation test entities

--- Hoeyer.OpcUa.TestEntities/Methods/IGantryMethods.cs
+++ Hoeyer.OpcUa.TestEntities/Methods/IGantryMethods.cs
@@ -5,7 +5,7 @@ namespace Hoeyer.opcUa.TestEntities.Methods;
 [OpcUaEntityMethods<Gantry>]
 public interface IGantryMethods
 {
-    public Task A(int q);
-    public Task<int> LetsGoInt(int a, float b, List<int> i);
+    public Task IntegerInput(int q);
+    public Task<int> MultiInputIntReturn(int a, float b, List<int> i);
     public Task<int> MoreMethods(int a, float b, float c, List<int> i);
 }
