805213cc4427239a5868f7d2da1956d01d42a727
rasmus <rasmus.hoeyer.hansen@gmail.com>
2025-07-26 16:18:24 +0200

    [Feature] Better adapter pattern

---
+++ Hoeyer.Common/Architecture/ILayerAdapter.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Hoeyer.Common.Architecture;
+
+public interface ILayerAdapter<in TAdaptionSource>
+{
+    public void Adapt(TAdaptionSource adaptionSource, IServiceCollection adaptionTarget);
+}
