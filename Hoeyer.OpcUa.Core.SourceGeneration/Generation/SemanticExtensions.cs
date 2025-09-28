32f5b1cd5bbff4f19426811f9ecf2738b0c93d0a
rasmus <rasmus.hoeyer.hansen@gmail.com>
2025-09-28 16:02:29 +0200

    Feature: Add BrowseNameAttribute, and use it to maintain a IBrowseNameCollection.cs for entities.

---
+++ Hoeyer.OpcUa.Core.SourceGeneration/Generation/SemanticExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Hoeyer.OpcUa.Core.SourceGeneration.Constants;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Hoeyer.OpcUa.Core.SourceGeneration.Generation;
+
+public static class SemanticExtensions
+{
+    public static string GetBrowseNameFromAttribute(this MemberDeclarationSyntax memberSyntax,
+        SemanticModel semanticModel, string fallBack)
+    {
+        var symbol = semanticModel.GetDeclaredSymbol(memberSyntax);
+        var firstArg = symbol?
+            .GetAttributes()
+            .FirstOrDefault(IsOpcEntityAttributeSymbol)
+            ?.ConstructorArguments.First();
+
+        if (firstArg is not { Value: string browseNameValue })
+        {
+            return fallBack;
+        }
+
+        return browseNameValue;
+    }
+
+    private static bool IsOpcEntityAttributeSymbol(AttributeData x) =>
+        WellKnown.FullyQualifiedAttribute
+            .BrowseNameAttribute
+            .WithGlobalPrefix
+            .Equals(x.AttributeClass?.ToDisplayString(SymbolDisplayFormats.FullyQualifiedNonGenericWithGlobalPrefix));
+}
