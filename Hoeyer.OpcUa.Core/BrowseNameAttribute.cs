058bd22ce4dd9558a0ed4a0f9ca7ff4afef62927
rasmus <rasmus.hoeyer.hansen@gmail.com>
2025-09-28 14:10:52 +0200

    Feature: Add BrowseNameAttribute, and use it to maintain a IBrowseNameCollection.cs for entities.

---
+++ Hoeyer.OpcUa.Core/BrowseNameAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Hoeyer.OpcUa.Core;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Enum)]
+public sealed class BrowseNameAttribute : Attribute
+{
+    public BrowseNameAttribute(string browseName)
+    {
+        if (string.IsNullOrEmpty(browseName))
+        {
+            throw new ArgumentOutOfRangeException(nameof(browseName) + " was null or empty.");
+        }
+
+        BrowseName = browseName;
+    }
+
+    public string BrowseName { get; }
+}
