d0c62e0bbe6553ad4dfdc1d8eac6f648312c30cd
rasmus <rasmus.hoeyer.hansen@gmail.com>
2026-01-06 19:02:44 +0100

    chore: remove unused code

--- Hoeyer.OpcUa.TestFramework/DependencyInjection/ServiceDescriptors/ServiceDescriptorExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-
-namespace Hoeyer.Opc.Ua.Test.TUnit.DependencyInjection.ServiceDescriptors;
-
-public static class ServiceDescriptorExtensions
-{
-    public static IEnumerable<ServiceDescriptor> Where(this IEnumerable<ServiceDescriptor> descriptors,
-        IPartialServiceMatcher matcher) => descriptors.Where(matcher.Equals);
-}
